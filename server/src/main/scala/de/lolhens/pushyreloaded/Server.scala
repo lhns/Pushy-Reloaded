0d9f3c27b01e9d14609ca2104418864eaeff301d
Scala Steward <scala-steward@users.noreply.github.com>
2022-07-03 10:39:39 +0000

    Applied Scalafix rule(s) github:http4s/http4s/v0_22?sha=series/0.22

--- server/src/main/scala/de/lolhens/pushyreloaded/Server.scala
+++ server/src/main/scala/de/lolhens/pushyreloaded/Server.scala
@@ -11,7 +11,7 @@ import org.http4s.dsl.task._
 import org.http4s.implicits._
 import org.http4s.scalatags._
 import org.http4s.server.Router
-import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.server.staticcontent.WebjarService.WebjarAsset
 import org.http4s.server.staticcontent._
 
